a84cf6398e5565d3e8ae91f164391f1369a0de6f
st55790 <tomas-o@seznam.cz>
2023-04-23 11:09:18 +0200

    add integration test + push

---
+++ src/test/java/cs/upce/fei/prudkytomas/cookbook/controller/IntegrationTest.java
@@ -0,0 +1,29 @@
+package cs.upce.fei.prudkytomas.cookbook.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetCategory() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/category"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Masová jídla"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Vegetariánská jídla"));
+    }
+}
